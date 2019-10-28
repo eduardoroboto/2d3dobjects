f81a7db09947f543721c6f9715f798c3cc4fd39a
sredroboto <eduardobarroso@edu.unifor.br>
2019-10-28 20:00:59 -0300

    O trabalho ate o momento

--- engine.py
+++ engine.py
@@ -6,9 +6,7 @@ from OpenGL.GLU import *
 from cube import *
 
 from shape2d import *
-
-
-
+from shape2d import Rectangle
 
 if __name__ == "__main__":
     pygame.init()
@@ -16,28 +14,46 @@ if __name__ == "__main__":
 
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
     # Pode utilizar o Perspective
-    gluPerspective(45, display[0]/display[1], 0.1, 50.0 )
+    gluPerspective(45, display[0]/display[1], 0.1, 500.0 )
     glTranslate(0.0, 0.0, -25)
-    quad = Triangle(0,0,5)
+
+
+    # quad2 = Square(0, 0, 7)
+    # quad = Rectangle(0, 0, 5,5)
+
 
     linex = Line(-10,0,10,0)
     liney = Line(0,-10,0,10)
-
-    # liney
+    # linez = Line(0,-10,0,10)
+    #quad = Triangle(0, 0, 5)
+    cc = Circle(0,0,2,20)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    print("clicado")
+                    #quad2.draw()
 
+
+        mouseMove = pygame.mouse.get_rel()
+
+        glRotate(mouseMove[0] * 0.2, 0.0, 1.0, 0.0)
+        glRotate(mouseMove[1] * 0.2, 1.0, 0.0, 0.0)
 
         # glRotate(1,0,0,1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         # cube()
-        quad.draw()
+        # quad.draw()
+        #quad2.draw()
+        cc.draw()
+
         linex.draw()
         liney.draw()
+
 
         pygame.display.flip()
         pygame.time.wait(10)
--- shape2d.py
+++ shape2d.py
@@ -1,23 +1,25 @@
 from OpenGL.GL import *
+from math import sin, cos, pi
+import copy
+
 
 class Vertex():
 
-    def __init__(self, x, y ):
-
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def get_list(self):
-        return [self.x,self.y]
+        return [self.x, self.y]
+
 
 class Line():
-    def __init__(self,x1,y1,x2,y2):
-
-        self.line = [Vertex(x1,y1), Vertex(x2,y2)]
+    def __init__(self, x1, y1, x2, y2):
+        self.line = [Vertex(x1, y1), Vertex(x2, y2)]
 
     def draw(self):
         glBegin(GL_LINES)
-        glColor3fv((1,1,1))
+        glColor3fv((1, 1, 1))
         for vertex in self.line:
             glVertex2fv(vertex.get_list())
 
@@ -26,36 +28,25 @@ class Line():
 
 class Triangle():
 
-    def __init__(self,x,y,width):
+    def __init__(self, x, y, width):
+        self.vertices = self.__create_edges(x, y, width)
 
-        self.vertices = self.__create_edges(x,y,width)
-
-
-
-    def __create_edges(self,x,y,width):
+    def __create_edges(self, x, y, width):
         points_list = list()
 
-
         points_list.append(Vertex(x, y))
-        points_list.append(Vertex(x+width, y))
-        points_list.append(Vertex(x + (width/2), y + width))
-
+        points_list.append(Vertex(x + width, y))
+        points_list.append(Vertex(x + (width / 2), y + width))
 
         return points_list
 
-
-
-
     def draw(self):
-
         glBegin(GL_TRIANGLES)
-        glColor3fv((0,1,0))
+        glColor3fv((0, 1, 0))
         for vertex in self.vertices:
             glVertex2fv(vertex.get_list())
 
         glEnd()
-
-
 
 
 class Square():
@@ -65,19 +56,17 @@ class Square():
         self.width = width
         self.points = self.__create_points()
 
-
     def __create_points(self):
         points_list = list()
 
-        points_list.append((self.x,self.y))
+        points_list.append((self.x, self.y))
         points_list.append((self.x + self.width, self.y))
         points_list.append((self.x + self.width, self.y + self.width))
-        points_list.append((self.x , self.y + self.width))
-
+        points_list.append((self.x, self.y + self.width))
 
         return points_list
-    def draw(self):
 
+    def draw(self):
         glBegin(GL_TRIANGLES)
 
         glVertex2fv(self.points[0])
@@ -91,10 +80,80 @@ class Square():
         glEnd()
 
 
-
-
 class Rectangle():
-    pass
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.points = self.__create_points()
+
+    def __create_points(self):
+        points_list = list()
+
+        points_list.append((self.x, self.y))
+        points_list.append((self.x + self.width, self.y))
+        points_list.append((self.x + self.width, self.y + self.height))
+        points_list.append((self.x, self.y + self.height))
+
+        return points_list
+
+    def draw(self):
+        glBegin(GL_TRIANGLES)
+
+        glVertex2fv(self.points[0])
+        glVertex2fv(self.points[1])
+        glVertex2fv(self.points[2])
+
+        glVertex2fv(self.points[2])
+        glVertex2fv(self.points[0])
+        glVertex2fv(self.points[3])
+
+        glEnd()
+
 
 class Circle():
-    pass
+
+    def __init__(self, x, y, radius, number,):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.number = number
+
+        self.vertices = self.__create_edges(self.x, self.y, self.radius,self.number)
+
+    def __create_edges(self, x, y, radius,number):
+        points_list = list()
+        # points_list.append(Vertex(x, y))
+        # points_list.append(Vertex(x + width, y))
+        # points_list.append(Vertex(x + width , y + width))
+        # point_one.x = point_one.x + radius * cos(i)
+        # point_one.y = point_one.y + radius * sin(i)
+
+        hx = copy.copy(x)
+        hy = copy.copy(y)
+        for i in range(0, number):
+            points_list.append(Vertex(hx, hy))
+            x = radius * cos(i)
+            y =  radius * sin(i)
+            points_list.append(Vertex(x, y))
+            x2 = radius * cos(i+1)
+            y2 = radius * sin(i+1)
+            points_list.append(Vertex(x2 , y2))
+
+
+        return points_list
+
+
+    def draw(self):
+
+        # glBegin(GL_LINES)
+        glBegin(GL_TRIANGLES)
+        # glColor4fv((1, 1, 1,1))
+        for vertex in self.vertices:
+            glVertex2fv(vertex.get_list())
+            print(vertex.get_list())
+
+
+        glEnd()
